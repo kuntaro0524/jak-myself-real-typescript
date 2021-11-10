70df5d3a1814f3bc201ff213ccf47aec89ffe5ec
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-10 21:47:21 +0900

    バグの温床になりうる propsで boolean を受け取ってそれを Todo componentの中で使っているのだが実際には呼び出し側から受け取っていないのに表示ができてしまうっていうお話

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,6 @@
 export const Todo = (props) => {
-  const { title, userid } = props;
-  return <p> {`${title}(user: ${userid})`} </p>;
+  const { title, userid, completed } = props;
+  // 完成か未完かを判断するbooleanを見て最初の文字を決める
+  const completeMark = completed ? "【完】" : "【未】";
+  return <p> {`${completeMark} ${title}(user: ${userid})`} </p>;
 };
