7c9cdfee38a12ccec50216fddd3c3fee84a43c3d
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-10 21:24:10 +0900

    型指定をすることのメリットがわかった １）マウスホバーで使うべき型がわかったり ２）取得したデータがおかしいかどうかわかったり ３）自動補完ツールがパラメータを出してくる→間違いが減る

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,23 @@ type TodoType = {
 };
 
 export default function App() {
-  // stateに対する型の指定方法　いったん <any> 型にしてみる
-  const [todos, setTodos] = useState<any>([]);
+  // stateに対する型の指定方法　ここも TodoTypeで指定
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      setTodos(res.data);
-    });
+    // TodoTypeのアレイ型の場合は以下のような指定方法
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <ChakraProvider>
       <div className="App">
         <Button onClick={onClickFetchData}> data acquisition </Button>
         {todos.map((todo) => (
-          <Todo key={todo.title} title={todo.title} userid={todo.userid} />
+          <Todo key={todo.title} title={todo.title} userid={todo.userId} />
         ))}
       </div>
     </ChakraProvider>
