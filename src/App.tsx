b87379a275c562e37ea43ab6a65989313e4220a5
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-10 21:03:50 +0900

    axiosで取得したデータを表示 axiosで取得してきたデータ→map関数で展開して <p> で表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 import axios from "axios";
+import { useState } from "react";
 
 export default function App() {
+  // stateに対する型の指定方法　いったん <any> 型にしてみる
+  const [todos, setTodos] = useState<any>([]);
+
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     });
   };
   return (
     <ChakraProvider>
       <div className="App">
         <Button onClick={onClickFetchData}> data acquisition </Button>
+        {todos.map((todo) => {
+          return <p> {todo.title} </p>;
+        })}
       </div>
     </ChakraProvider>
   );
