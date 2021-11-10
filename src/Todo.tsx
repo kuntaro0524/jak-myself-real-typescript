128c06a29c93b319cc60cf96190467797cd938bb
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-11 08:31:44 +0900

    propsも型指定すると間違いが減る propsにTodoTypeをアサイン

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,12 @@
-export const Todo = (props) => {
-  const { title, userid, completed } = props;
+type TodoType = {
+  userId: number;
+  title: string;
+  completed: boolean;
+};
+
+export const Todo = (props: TodoType) => {
+  const { title, userId, completed } = props;
   // 完成か未完かを判断するbooleanを見て最初の文字を決める
   const completeMark = completed ? "【完】" : "【未】";
-  return <p> {`${completeMark} ${title}(user: ${userid})`} </p>;
+  return <p> {`${completeMark} ${title}(user: ${userId})`} </p>;
 };
