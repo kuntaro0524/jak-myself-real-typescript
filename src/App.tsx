ecddbc53af43e55cc3bc821bfd83b8db4be0a8c0
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-10 20:56:34 +0900

    axiosでデータを取得してきた

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Box, Button, ChakraProvider } from "@chakra-ui/react";
-// import "./styles.css";
+import axios from "axios";
 
 export default function App() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
   return (
     <ChakraProvider>
       <div className="App">
-        <Box bg="tomato">
-          <Button colorScheme="green" bg="#ff0000" _hover={{ bg: "#00ff00" }}>
-            Fuck you
-          </Button>
-        </Box>
+        <Button onClick={onClickFetchData}> data acquisition </Button>
       </div>
     </ChakraProvider>
   );
