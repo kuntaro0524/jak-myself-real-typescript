7af62281f6ca970bd9e1f4f1b5fe57f9ba19b795
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-11 22:38:24 +0900

    FCを使うのか？ 関数コンポーネントの定義のときにFCという数値を入れるかどうか？

--- src/Text.tsx
+++ src/Text.tsx
@@ -1,9 +1,15 @@
+import { FC } from "react";
+
 type Props = {
   color: string;
   fontSize: string;
 };
 
-export const Text = (props: Props) => {
+// 実際には以下の書き方のほうが一般的
+// Functional Component として定義してあげる
+// その場合には props: Props みたいな型指定ではなく
+// ところがこのFCは「暗黙のうちに」 childrenを受け取る仕様になっていてバグの温床になりそう
+export const Text: FC<Props> = (props) => {
   const { color, fontSize } = props;
   // スタイルを当てるとき、変数名とprops名が同じならばそのまま書ける
   return <p style={{ color, fontSize }}> Text death. </p>;
