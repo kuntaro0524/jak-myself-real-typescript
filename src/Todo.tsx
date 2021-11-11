995b20f219f230c6f78ca310d5cba27501d5ac50
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-11 22:22:00 +0900

    別ファイルに型定義を分離２ 型定義を分離したとき、ある props　の中の型だけを無視して渡したいときようの Omit

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,9 +1,7 @@
 import { TodoType } from "./types/todo";
 
 // todoで定義したTodoTypeの中身の一部だけを props で利用したいときの記述の例１
-export const Todo = (
-  props: Pick<TodoType, "userId" | "title" | "completed">
-) => {
+export const Todo = (props: Omit<TodoType, "id">) => {
   const { title, userId, completed } = props;
   // 完成か未完かを判断するbooleanを見て最初の文字を決める
   const completeMark = completed ? "【完】" : "【未】";
