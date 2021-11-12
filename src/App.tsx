931365556f30d995e31c38ea76692bf92c1e1c44
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-12 09:00:33 +0900

    Userという型を指定して UserProfileで表示 するという機能を実装してみた （オプショナルチェイニングのために）

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import { useState } from "react";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { User } from "./types/user";
+import { UserProfile } from "./UserProfile";
+
+// ユーザのプロファイルを一つ定義してみる
+const user: User = {
+  name: "平田邦生",
+  hobbies: ["ゲーム", "映画"]
+};
 
 export default function App() {
   // stateに対する型の指定方法　ここも TodoTypeで指定
@@ -21,6 +29,8 @@ export default function App() {
   return (
     <ChakraProvider>
       <div className="App">
+        {/* 設定したユーザのプロファイルを表示する */}
+        <UserProfile user={user} />
         <Text color="red" fontSize="18px" />
         <Button onClick={onClickFetchData}> data acquisition </Button>
         {todos.map((todo) => (
---
+++ src/types/user.ts
@@ -0,0 +1,5 @@
+// 型を指定するときは const ではなくて type ですよ！
+export type User = {
+  name: string;
+  hobbies?: Array<string>;
+};
